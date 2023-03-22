a289a00627acddaf708e355b6e15537a8205bba2
elian <elian118@naver.com>
2023-03-22 12:53:26 +0900

    환경변수 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 import 'package:pkcoin/slider_widget.dart';
 import 'package:pkcoin/widgets/buttons.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:web3dart/web3dart.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
@@ -39,7 +41,13 @@ class _MyHomePageState extends State<MyHomePage> {
   late Web3Client ethClient;
   bool data = false;
   int myAmount = 0;
-  final myAddress = "0x8405368D4231600517BAE5F2aFCfefb89d7fD1F2";
+  // 메타마스크 계정 주소
+  final myAddress = dotenv.env['METAMASK_ADDRESS'];
+  // 리믹스 IDE -> Deployed Contracts 바로 밑에 있는 해시코드 복붙
+  //  -> 이 해시코드는 솔리디티 코드가 배포 완료된 이후에 확인 가능하다.
+  final contractAddr = dotenv.env['CONTRACT_ADDRESS'];
+  final infuraKey = dotenv.env['INFURA_API_KEY'];
+  final myPrivateKey = dotenv.env['METAMASK_PRIVATE_KEY'];
 
   String txHash = '';
   var myData;
@@ -51,10 +59,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // 원래는 메인넷으로 서비스해야 하나, 테스트넷 설정은 살짝 바꿔줘야 한다.
     // 인퓨라 -> 연결된 프로젝트 엔드포인트 -> 이더리움 -> 'gorli 테스트넷'으로 지정 후 뜬 주소를 여기 입력
     ethClient = Web3Client(
-      "https://goerli.infura.io/v3/4d411fea2a97487cb0f0ec024c8b6422",
+      'https://goerli.infura.io/v3/$infuraKey',
       httpClient,
     );
-    getBalance(myAddress);
+    getBalance(myAddress!);
   }
 
   // JSON 형식으로 컴파일된 스마트컨트렉트 코드 불러오기
@@ -62,12 +70,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // 리믹스 IDE -> 솔리디티 컴파일러 -> Compilation Details 버튼 오른쪽 밑 -> ABI 클릭(JSON 형식으로 코드 복사)
     //  -> 프로젝트에 assets/abi.json 경로 및 파일 생성하고 코드 복붙
     String abi = await rootBundle.loadString("assets/abi.json");
-    // 리믹스 IDE -> Deployed Contracts 바로 밑에 있는 해시코드 복붙
-    //  -> 이 해시코드는 솔리디티 코드가 배포 완료된 이후에 확인 가능하다.
-    String contractAddress = "0xD97ed3F91b21f4b48856360a809C8870FF8b49cB";
 
     final contract = DeployedContract(ContractAbi.fromJson(abi, "PKCoin"),
-        EthereumAddress.fromHex(contractAddress));
+        EthereumAddress.fromHex(contractAddr!));
 
     return contract;
   }
@@ -97,8 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
   // 제출 -> 잔고 조회(갱신), 입금, 인출
   Future<String> submit(String functionName, List<dynamic> args) async {
     // 테스트를 위해 메타마스크로부터 계정 비공개 키를 하드코드로 입력. api 안 씀
-    EthPrivateKey credentials = EthPrivateKey.fromHex(
-        "580f158bbd3f504cc98d02dee5d70a4be939376b3336831cd42fa53b7f9b1e04");
+    EthPrivateKey credentials = EthPrivateKey.fromHex(myPrivateKey!);
 
     DeployedContract contract = await loadContract();
     final ethFunction = contract.function(functionName);
@@ -179,7 +183,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Buttons(
             getBalance: getBalance,
-            myAddress: myAddress,
+            myAddress: myAddress!,
             sendCoin: sendCoin,
             withdrawCoin: withdrawCoin,
           ),
